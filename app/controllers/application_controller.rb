e0906b1bc8a0742e0cd4fab5878547016a796c8e
tanvk1ch1 <moko2.ss0828@gmail.com>
2019-01-13 16:10:14 +0900

    push

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :basic_auth
+
+private
+
+# RailsアプリへのBasic認証導入用,環境変数
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
+  end
 end
