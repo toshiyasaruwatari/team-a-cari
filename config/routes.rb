ae429cb8b3392fc4c3d5878272de92616f3b41c7
muneichi <yamaguchisoichiro@yamaguchisouichirounoMacBook-ea.local>
2019-01-18 18:55:44 +0900

    購入確認ページ マークアップ

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
-
+  get 'items/new' => 'items#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
