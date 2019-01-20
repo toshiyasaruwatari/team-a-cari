a5a336ba90af783c801f0995b763474134f0d7eb
alexandrokuta <me.gusta.la.regater@gmail.com>
2019-01-20 19:50:23 +0900

    enum 追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,6 @@
 
 require "csv"
 
-CSV.foreach('db/category.csv') do |row|
-  Category.create(id: row[0], name: row[1], ancestry: row[2])
+CSV.foreach('db/item.csv') do |row|
+  Item.create(id: row[0], name: row[1], status: row[2])
 end
