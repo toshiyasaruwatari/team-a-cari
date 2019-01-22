7e6f1c44e5ed5d218b83c12f2004e8fd7d624bc0
alexandrokuta <me.gusta.la.regater@gmail.com>
2019-01-22 20:20:40 +0900

    トップページ　表示実装(カテゴリー、ブランド別）

---
+++ db/migrate/20190122065220_add_colmun_category_to_items.rb
@@ -0,0 +1,4 @@
+class AddColmunCategoryToItems < ActiveRecord::Migration[5.1]
+  def change
+  end
+end
---
+++ db/migrate/20190122070617_add_references_to_category.rb
@@ -0,0 +1,4 @@
+class AddReferencesToCategory < ActiveRecord::Migration[5.1]
+  def change
+  end
+end
