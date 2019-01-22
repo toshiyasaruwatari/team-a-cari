46def528ecca8db9f1433ca699c1d851a4c7689d
muneichi <yamaguchisoichiro@yamaguchisouichirounoMacBook-ea.local>
2019-01-22 20:02:48 +0900

    Userモデルのテスト

---
+++ spec/factories/users.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+
+  factory :user do
+    nickname              "mercari"
+    email                 "mercari@gmail.com"
+    password              "00000000"
+    password_confirmation "00000000"
+  end
+
+end
