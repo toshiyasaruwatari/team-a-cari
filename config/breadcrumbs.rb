8cc23edca9d124be09d8ed64861d57ae9588393d
alexandrokuta <me.gusta.la.regater@gmail.com>
2019-01-26 20:56:10 +0900

    コンフリクト解消

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -3,36 +3,36 @@ crumb :root do
 end
 
 crumb :mypage do
-  link "マイページ", "/h"
+  link "マイページ", "/users/show"
   parent :root
 end
 
 crumb :card do
-  link "ログアウト", "/a"
+  link "ログアウト", "/users/:id/pay_way"
   parent :mypage
 end
 
 crumb :card_add do
-  link "支払方法", "/b"
+  link "支払方法", "/users/:id/add_card"
   parent :mypage
 end
 
 crumb :detail do
-  link "商品詳細ページ", "/c"
+  link "商品詳細ページ", "/items/show"
   parent :root
 end
 
 crumb :edit do
-  link "本人情報の登録", "d"
+  link "本人情報の登録", "/users/:id/~"
   parent :mypage
 end
 
 crumb :identification do
-  link "本人情報の入力", "e"
+  link "本人情報の入力", "/users/:id/identify"
   parent :mypage
 end
 
 crumb :logout do
-  link "ログアウト", "g"
+  link "ログアウト", "/users/:id/logout"
   parent :mypage
 end
