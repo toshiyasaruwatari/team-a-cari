c34c628a9addb8fedd856a2bd72cc403ab61037c
muneichi2 <muneichi.x07@gmail.com>
2019-02-04 20:55:57 +0900

    コンフリクト修正①

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -1,24 +1,24 @@
 $(document).on('turbolinks:load', function(){
 
   $('.btn-default.btn-red').on('click', function(){
-    $('#overlay').fadeIn();
-    $('#modal__window').fadeIn();
+    $('.overlay').fadeIn();
+    $('.modal').fadeIn();
     $('html, body').css('overflow', 'hidden');
   });
 
-  $('#overlay').on('click', function(){
-    $('#overlay').fadeOut();
-    $('#modal__window').fadeOut();
+  $('.overlay').on('click', function(){
+    $('.overlay').fadeOut();
+    $('.modal').fadeOut();
     $('html, body').removeAttr('style');
   });
 
-  $('#modal__cancel-btn').on('click', function(){
-    $('#overlay').fadeOut();
-    $('#modal__window').fadeOut();
+  $('.modal__btn--cancel').on('click', function(){
+    $('.overlay').fadeOut();
+    $('.modal').fadeOut();
     $('html, body').removeAttr('style');
   });
 
-  $('#modal__delete-btn').on('click', function(){
+  $('.modal__delete--btn').on('click', function(){
     $('html, body').removeAttr('style');
   });
 });
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'users/registrations' }
 
   devise_scope :user do
     delete :sign_out, to: 'devise/sessions#destroy'
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   root to: 'items#index'
 
-  resources :users, only: [:show, :edit, :update, :destroy, :new, :create, :index] do
+  resources :users do
     collection do
       get '/:id/identify', to: 'users#identify'
       get '/:id/logout', to: 'users#logout'
@@ -18,33 +18,22 @@ Rails.application.routes.draw do
       get '/:id/bought', to: "items#bought"
       get '/:id/registrate', to: "cards#registrate"
     end
+    resources :reviews, only: [:new, :create]
     resources :cards, only:[:index,:new, :pay]
       collection do
         post '/pay', to: "cards#pay"
       end
   end
 
-  resources :items, only: [:index, :show, :new, :create, :edit, :update, :destroy] do
+  resources :items do
     collection do
       get '/:id/buy', to: 'items#buy'
       get '/:id/buy_confirm', to: 'cards#buy_confirm'
       post '/:id/buy', to: 'cards#buy'
       get '/:id/change', to: 'items#change'
       delete '/:id/change', to: 'items#destroy'
-    end
-  end
-  resources :users, only:[:show] do
-    resources :cards, only:[:index,:new, :pay]
-      collection do
-        post '/pay', to: "cards#pay"
-      end
-  end
-  get '/trade/sell', to: "items#trade_sell"
-  get '/trade/now', to: "items#trade_now"
-  delete '/trade/now', to: "items#trade_now"
-  get '/trade/sold', to: "items#trade_sold"
+      delete '/trade/now', to: "items#trade_now"
     end
   end
   patch '/:id/user/identify', to: 'users#identify'
 end
-
