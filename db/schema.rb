cc6d497f23de94d45bb12dcd3626da3d0c940693
tanvk1ch1 <moko2.ss0828@gmail.com>
2019-02-04 20:48:45 +0900

    軽微な修正
    what
    記述ミスの修正
    why
    image_tagとlink_toは分けて記述する必要があるため

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190131063129) do
+ActiveRecord::Schema.define(version: 20190202081450) do
 
   create_table "brands", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name", null: false
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 20190131063129) do
     t.string "name", null: false
     t.string "ancestry"
     t.index ["ancestry"], name: "index_categories_on_ancestry"
+  end
+
+  create_table "comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer "user_id"
+    t.integer "item_id"
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "item_images", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
